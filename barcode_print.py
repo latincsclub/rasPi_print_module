d04627cba1faa86cdb599a5da22c1f0430037986
Vedant Pathak <vedantdpathak@iCloud.com>
2018-02-26 20:06:53 -0600

    Update barcode_print.py

--- barcode_print.py
+++ barcode_print.py
@@ -4,10 +4,14 @@ from barcode.writer import ImageWriter
 from PIL import Image
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+form reportlab.platypus import Image, SimpleDocTemplate
+from reportlab.lib.styles import getSampleStylesheet
 
 conn = cups.Connection()
 printers = conn.getPrinters()
 printer_name = printers.keys()[0]
+
+doc = SimpleDocTemplate("printout.pdf")
 
 app = Flask(__name__)
 CORS(app)
